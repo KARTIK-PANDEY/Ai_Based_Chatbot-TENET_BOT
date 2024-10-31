76bc8505681191a6fb1af5528080154078a0a846
KARTIK PANDEY <kartik0pandey00@gmail.com>
2024-10-31 16:51:55 +0530

    Add files via upload

---
+++ msg app.py
@@ -0,0 +1,48 @@
+import os
+import gradio as gr
+from groq import Groq
+
+# Fetch API key from environment variable
+api_key = "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"
+
+client = Groq(api_key=api_key)
+
+def chatbot(message, history):
+    messages = [{"role": "system", "content": "You are a helpful assistant."}]
+    for human, assistant in history:
+        messages.append({"role": "user", "content": human})
+        messages.append({"role": "assistant", "content": assistant})
+    messages.append({"role": "user", "content": message})
+
+    completion = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=messages,
+        temperature=0.7,
+        max_tokens=1024,
+        top_p=1,
+        stream=True,
+    )
+
+    partial_message = ""
+    for chunk in completion:
+        if chunk.choices[0].delta.content is not None:
+            partial_message += chunk.choices[0].delta.content
+            yield partial_message
+
+iface = gr.ChatInterface(
+    chatbot,
+    chatbot=gr.Chatbot(height=600),
+    textbox=gr.Textbox(placeholder="Type your message here...", container=False, scale=7),
+    title="KARTIK's BOT",
+    description="Chat with kartik 6.12b LLM ",
+    theme="soft",
+    examples=[
+        "What is Data Science?",
+        "Can you explain Stock market?",
+        "Tell me a joke about programming.",
+    ],
+    cache_examples=True
+)
+
+if __name__ == "__main__":
+    iface.launch(share=True)
